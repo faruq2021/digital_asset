a191a87f392294cd0674b650973951c74a1d4ec2
Dauda <umardauda@rocketmail.com>
2022-12-08 10:19:15 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,70 +1,108 @@
+import React from 'react';
+// import digiasset from './digiasset.png';
+// import enaira from './enaira.jpeg';
+import Navbar from './pages/navbar';
+import Hero from './pages/hero';
+
+
+// console.log(digiasset); // /logo.84287d09.png
+
+
 function App() {
+	
 	return (
-		<div className="min-h-screen flex flex-col text-white">
-			<main className="container mx-auto px-6 pt-16 flex-1 text-center">
+	<div>
+	<Navbar />
+	<Hero/>
+	</div>
+		
+		// 		<div className="min-h-screen flex flex-col text-white">
 
-				<h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Welcome to</h2>
-				<h1 className="text-2xl md:text-4xl lg:text-6xl uppercase font-black mb-8">
-          Your Goto for Digital Asset management and tracking</h1>
+		// 	<div class="header">
+		// 		<a href="#default" class="logo"><img src={digiasset} alt="Logo" align="left"/></a>
+		// 		<div class="header-right">
+		// 			<a class="active" href="#home">Home</a>
+		// 			<a href="#Registration link">Sign Up</a>
+		// 			<a href="#about">About</a>
+					
+		
+		// 		</div>
+		// 	</div>
 
-				<p className="text-base md:text-lg lg:text-2xl mb-8">
-          A System that helps handle financial transparency in Governance, through blockchain!</p>
+		// 	<main className="container mx-auto px-6 pt-16 flex-1 text-center">
+		// 		<h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Welcome to</h2>
+		// 		<h1 className="text-2xl md:text-4xl lg:text-6xl uppercase font-black mb-8">
+		// 			Your Goto for Digital Asset management and tracking</h1>
+				
 
-				<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 
-        lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
-					All 37 states including Fct, subsrcibed for state Executive assets management
-				</div>
+		// 		<p className="text-base md:text-lg lg:text-2xl mb-8">
+		// 			A System that helps handle financial transparency in Governance, through blockchain!</p>
 
-				<form 
-					action="#" 
-					method="post" 
-					id="revue-form"
-					name="revue-form"
-					target="_blank">
 
-					<div className="flex flex-col md:flex-row justify-center mb-4">
-						<input
-							placeholder="Your wallet address..."
-							type="email"
-							name="member[email]"
-							id="member_email"
-							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12
-               bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none 
-               rounded-full outline-none mb-4 md:mb-0"
-						/>
-						<input
-							type="submit"
-							value="Subscribe Today"
-							name="member[subscribe]"
-							id="member_submit"
-							className="bg-primary md:rounded-tl-none md:rounded-bl-none rounded-full 
-              text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
-						/>
-					</div>
+		// 		<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 
+        // lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+		// 			All 37 states including Fct, subsrcibed for state Executive asset management
 
-					<div className="opacity-75 italic">
-						By subscribing, you agree with our terms and conditions <a target="_blank" href="#" 
-            className="hover:opacity-80 duration-150">Regarding blockchain accesibility</a> 
-            and <a target="_blank" href="#" className="hover:opacity-80 duration-150">Privacy Policy</a>.
-					</div>
+		// 		</div>
 
-				</form>
-			</main>
 
-			<footer className="container mx-auto p-6">
-				<div className="flex flex-col md:flex-row items-center justify-between">
-					<p className="mb-4 md:mb-0 md:text-xl">Built with &#128545; Love by Umar Faruq</p>
+		// 		<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 
+        // lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+		// 			All 37 states including Fct, subsrcibed for state Executive asset management
+		// 			<img src={digiasset} alt="Logo" align="center"/>
+		// 		</div>
 
-					<div className="flex -mx-6">
-						<a href="#" className="mx-3 hover:opacity-80 duration-150">About DigiAsset</a> | 
-						<a href="#" className="mx-3 hover:opacity-80 duration-150">Public Assets and CBDCs</a> | 
-						<a href="#" className="mx-3 hover:opacity-80 duration-150">Contact Us</a>
-					</div>
-				</div>
-			</footer>
-		</div>	
+
+
+		// 		<form
+		// 			action="#"
+		// 			method="post"
+		// 			id="revue-form"
+		// 			name="revue-form"
+		// 			target="_blank">
+
+		// 			<div className="flex flex-col md:flex-row justify-center mb-4">
+		// 				<input
+		// 					placeholder="Enaira for the win..."
+		// 					type="email"
+		// 					name="member[email]"
+		// 					id="member_email"
+		// 					className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12
+        //        bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none 
+        //        rounded-full outline-none mb-4 md:mb-0"
+		// 				/>
+		// 				<input
+		// 					type="submit"
+		// 					value="Find out more about CBDCs"
+		// 					name="member[subscribe]"
+		// 					id="member_submit"
+		// 					className="bg-primary md:rounded-tl-none md:rounded-bl-none rounded-full 
+        //       text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
+		// 				/>
+		// 			</div>
+
+		// 			<div className="opacity-75 italic">
+		// 				Enaira the future of cashless economy<a target="_blank" href="#"
+		// 					className="hover:opacity-80 duration-150"></a>
+		// 				and <a target="_blank" href="#" className="hover:opacity-80 duration-150"></a>.
+		// 			</div>
+
+		// 		</form>
+		// 	</main>
+
+		// 	<footer className="container mx-auto p-6">
+		// 		<div className="flex flex-col md:flex-row items-center justify-between">
+		// 			<p className="mb-4 md:mb-0 md:text-xl">Built with &#128545; Love by Umar Faruq</p>
+
+		// 			<div className="flex -mx-6">
+		// 				<a href="#" className="mx-3 hover:opacity-80 duration-150">About DigiAsset</a> |
+		// 				<a href="#" className="mx-3 hover:opacity-80 duration-150">Public Assets and CBDCs</a> |
+		// 				<a href="#" className="mx-3 hover:opacity-80 duration-150">Contact Us</a>
+		// 			</div>
+		// 		</div>
+		// 	</footer>
+		// </div>
 	)
 }
 
 export default App
- 
